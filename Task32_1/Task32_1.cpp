77f8fe60bd36ece246ccc2d1029921411711554e
Dmitry <filchev_dmitry@mail.ru>
2023-05-07 12:36:32 +0300

    Добавьте файлы проекта.

---
+++ Task32_1/Task32_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include<string>
+#include<fstream>
+#include"nlohmann/json.hpp"
+
+int main()
+{
+	nlohmann::json film=
+	{
+		{"movie","The_Terminator"},
+		{"country","USA"},
+		{"year",1984},
+		{"film_company","Hemdale_Film"},
+		{"screenwriter","James Cameron"},
+		{"director","James Cameron"},
+		{"producer","Gail Ann Hurd"},
+		{"Terminator","Arnold Schwarzenegger"},
+		{"Sarah Connor","Linda Hamilton"},
+		{"Kyle Reese","Michael Bean"}
+	};
+	std::ofstream file("drom.json");
+	file << film;
+
+	return 0;
+}
